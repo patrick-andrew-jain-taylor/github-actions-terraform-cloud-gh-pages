5ec740f201cc4b744d8340d74723dcf19237f718
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-02-28 16:47:05 -0500

    Changing to v0.1.4.

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "resume" {
   source  = "patrick-andrew-jain-taylor/pages/github"
-  version = "0.1.3"
+  version = "0.1.4"
   description = "This is my personal resume."
   repository_name = "resume"
   root_domain = "jaintaylor.family"
