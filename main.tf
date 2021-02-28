86957ae4fe399b9924feeb6b706955a3a6765dc6
Patrick Jain-Taylor <patrick.andrew.taylor@gmail.com>
2021-02-28 16:18:30 -0500

    feat: Using github pages module.

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-data "github_branch" "main" {
-  branch = "main"
-  repository = "terraform-github-pages"
-}
-
-output "branch" {
-  value = data.github_branch.main.ref
+module "resume" {
+  source  = "patrick-andrew-jain-taylor/pages/github"
+  version = "0.1.0"
+  description = "This is my personal resume."
+  repository_name = "resume"
+  root_domain = "jaintaylor.family"
+  subdomain = "resume.patrick"
 }
